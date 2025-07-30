e2f60ebb501fd073b8d3ab1658940eca9890906c
Julian Vergel de Dios <julian.vergeldedios@gmail.com>
2025-07-30 09:59:24 -0700

    adding version and logging to startup

--- packages/backend/index.ts
+++ packages/backend/index.ts
@@ -1,7 +1,11 @@
 import { startServer } from "./src/server";
 import { config } from "./src/config";
 import { getLogger } from "./src/logging";
+import { version } from "./package.json";
 
-getLogger().info("Environment initialized", JSON.stringify(config));
+const logger = getLogger();
+
+logger.info(`Starting Coeur v${version}`);
+logger.info("Environment initialized", JSON.stringify(config));
 
 startServer();
