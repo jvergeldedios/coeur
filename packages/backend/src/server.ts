4ac3b97a4c0c6125e0d991aa2d126298e1f904c0
Julian Vergel de Dios <julian.vergeldedios@gmail.com>
2025-07-31 01:12:23 -0700

    adding healthcheck endpoint

--- packages/backend/src/server.ts
+++ packages/backend/src/server.ts
@@ -11,6 +11,7 @@ const app = new Hono<{ Variables: Variables }>();
 app.use("*", loggerMiddleware);
 
 app.get("/", (c) => c.text("Hello World"));
+app.get("/healthz", (c) => c.text("OK"));
 app.route("/api", api);
 
 export { app };
