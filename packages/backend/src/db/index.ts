c35aa93ac6f9dea6d8e0acd5b899935a5375ffaa
Julian Vergel de Dios <julian.vergeldedios@gmail.com>
2025-07-31 01:52:08 -0700

    adding query logging

--- packages/backend/src/db/index.ts
+++ packages/backend/src/db/index.ts
@@ -1,6 +1,23 @@
-import { drizzle } from "drizzle-orm/bun-sql";
 import { SQL } from "bun";
+import { drizzle } from "drizzle-orm/bun-sql";
+import type { Logger } from "drizzle-orm";
 
 import { config } from "../config";
+import { getLogger } from "../logging";
 
-export const db = drizzle(new SQL(config.DATABASE_URL));
+class DBLogger implements Logger {
+  logQuery(query: string, params: unknown[]) {
+    getLogger()
+      .child()
+      .withContext({
+        query,
+        params,
+      })
+      .info("Executing query");
+  }
+}
+
+export const db = drizzle({
+  client: new SQL(config.DATABASE_URL),
+  logger: new DBLogger(),
+});
